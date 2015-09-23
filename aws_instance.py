5aa25bbead8750491a238194ad961446ec8f3196
bangi123 <bangi_111@yahoo.com>
2015-09-23 00:31:56 -0500

    adding comments

--- aws_instance.py
+++ aws_instance.py
@@ -1,17 +1,30 @@
+# import boto module 
+
 import boto 
 import boto.ec2
+
+# connect to boto ec2 instance  
 conn = boto.ec2.connect_to_region("us-west-2",
      aws_access_key_id='AKIAJ76RNMXT3A4W4M5A',
      aws_secret_access_key='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')
 print "conn is %s" % conn
+# get status of all instances from conn 
 statuses = conn.get_all_instance_status()
 print "status of instances %s" % statuses 
+
+#get instances from reservations 
 reservations = conn.get_all_instances()
+
 print "****************************************"
 print " reservations are %s" % reservations 
 print "****************************************"
+# print the id of the instance 
 print "reservations id is %s " % reservations[0].instances[0].id 
+# print the state of an instance 
 print "reservations state is %s " % reservations[0].instances[0].state
-reservations[0].instances[0].stop()
+
+# To stop an instance 
+#reservations[0].instances[0].stop()
+
 #print "reservation name is %s " %reservations[0].instances[0].name 
 print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
